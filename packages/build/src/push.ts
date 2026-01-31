f266978ff645383af1d0d612b15b4a9774f1dd48
Seaszn <111234706+seaszn@users.noreply.github.com>
2026-01-31 19:47:04 +0100

    pfxes

--- packages/build/src/push.ts
+++ packages/build/src/push.ts
@@ -1,4 +1,4 @@
 import { execSync } from "child_process";
 
 execSync("clasp push -f", { stdio: "inherit" });
-execSync("clasp run __init", { stdio: "inherit" });
+// execSync("clasp run __init", { stdio: "inherit" });
